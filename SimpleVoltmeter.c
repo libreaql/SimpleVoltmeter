9a647c666a07b658091e05cdf4534ac6b33b36d1
Sami Hammal <libreaql@gmail.com>
2013-05-25 13:05:00 +0300

    Initial Commit

---
+++ SimpleVoltmeter.c
@@ -0,0 +1,19 @@
+/*
+ * SimpleVoltmeter.c
+ *
+ * Created: 25/5/2013 12:54:26
+ *  Author: Sam
+ */ 
+
+
+#include <avr/io.h>
+#include "SimpleVoltmeter.h"
+
+int main(void) {
+
+    while(1) {
+
+        //TODO:: Please write your application code
+		
+    }
+}
---
+++ SimpleVoltmeter.h
@@ -0,0 +1,21 @@
+/*
+ * SimpleVoltmeter.h
+ *
+ * Created: 25/5/2013 12:54:48
+ *  Author: Sam
+ *
+ *  Description:
+ *  This is a simple voltmeter using AVR ATmega8.
+ *  The DC voltage is sampled with the ADC and sent to the PC via the serial
+ *  port where a MATLAB GUI plots the acquired values
+ */ 
+
+
+#ifndef SIMPLEVOLTMETER_H_
+#define SIMPLEVOLTMETER_H_
+
+
+
+
+
+#endif /* SIMPLEVOLTMETER_H_ */
